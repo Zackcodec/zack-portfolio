a5d7e8f58d2a21318c722fb691c8314ae5d3eefe
Zackcodec <htthakur5555@gmail.com>
2025-08-26 02:38:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+AOS.init();
+
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
